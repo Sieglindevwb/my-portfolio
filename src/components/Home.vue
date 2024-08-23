4225952285b07162ccc86e45b325a3a1742a270f
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-08-23 10:13:54 +0200

    Style homepage

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Work</h1>
+  <h1>Projects</h1>
+  <h1>About</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-family: 'UnifrakturMaguntia', cursive;
+  font-weight: 400;
+  font-style: normal;
+}
+</style>
